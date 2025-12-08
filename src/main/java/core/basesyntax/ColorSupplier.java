f7125d13d57cd6de07de59ed9163736f1cd10c55
iryna <popovicirina40@gmail.com>
2025-12-08 23:45:11 +0000

    delete redundant space

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -2,14 +2,12 @@ package core.basesyntax;
 
 import java.util.Random;
 
-import static core.basesyntax.Colors.*;
-
 public class ColorSupplier {
 
     private static final Random RANDOM = new Random();
 
     public Colors getRandomColor() {
-        Colors[] values = values();
+        Colors[] values = Colors.values();
         return values[RANDOM.nextInt(values.length)];
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,13 +1,12 @@
 package core.basesyntax;
 
-import java.awt.*;
 import java.util.Random;
 
 public class Lottery {
 
     private static final int MAX_NUMBER = 100;
     private final Random rundom = new Random();
-    ColorSupplier colorSupplier = new ColorSupplier();
+    private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
 
